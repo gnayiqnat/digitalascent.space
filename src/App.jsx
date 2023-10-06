9e2ff85f7393cfdc5119f94c1d08f97ad1905f02
Digital Ascent <digiasce@gmail.com>
2023-10-06 20:28:56 +0800

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css';
+import Nav from './sec/navigation/navigation'
+import handleNavClick from './sec/navigation/navigation'
+import Section1 from './sec/section1'
+
+function App() {
+  return (
+    <div id='container'>
+    <Nav/>
+    <Section1 />
+    <h1>test</h1>
+    </div>
+  );
+}
+
+
+export default App;
---
+++ src/sec/section1.jsx
@@ -0,0 +1,10 @@
+
+
+function Section1 () {
+
+
+
+    return <h2>section123</h2>
+}
+
+export default Section1
