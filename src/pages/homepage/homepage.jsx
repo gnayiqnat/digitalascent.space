c38dcf260526c23e51dee17bbc3b762617750ede
gnayiqnat <gnayiqnat@gmail.com>
2023-10-29 17:01:51 +0800

    Remove repeating code in homepage

--- src/pages/homepage/homepage.jsx
+++ src/pages/homepage/homepage.jsx
@@ -24,7 +24,6 @@ import {
 } from '@mui/material';
 import { motion, useAnimationControls } from 'framer-motion';
 import { useState } from 'react';
-import { render } from 'react-dom';
 import { useMediaQuery } from 'react-responsive';
 
 function Homepage(props) {
@@ -40,88 +39,71 @@ function Homepage(props) {
 }
 
 function Section1() {
+	const [hasRun, setHasRun] = useState(false);
+	const top = useAnimationControls();
+	const bottom = useAnimationControls();
 	const isMobile = useMediaQuery({ query: '(max-width: 768px)' });
+	function handleHover() {
+		if (hasRun === false) {
+			top.start({ x: -100 });
+			bottom.start({ x: 100 });
+			setHasRun(true);
+		} else {
+			top.start({ x: 0 });
+			bottom.start({ x: 0 });
+			setHasRun(false);
+		}
+	}
 
 	return (
-		<>
-			<Container
-				component='main'
-				maxWidth='sm'
-				sx={{
-					display: 'flex',
-					alignItems: 'center',
-					justifyContent: 'center',
-					marginTop: 14,
-				}}
-			>
-				{isMobile ? (
-					<Box>
-							<DigitalAscent />
-					</Box>
-				) : (
-					<motion.div
-						width={300}
-						height={200}
+		<Container
+			component='main'
+			maxWidth='sm'
+			sx={{
+				display: 'flex',
+				alignItems: 'center',
+				justifyContent: 'center',
+				userSelect: 'none',
+				marginTop: 14,
+			}}
+		>
+			{isMobile ? (
+				<Box>
+					<Typography sx={headerStyle}>
+						DIGITAL <Box marginLeft={3}>ASCENT</Box>
+					</Typography>
+				</Box>
+			) : (
+				<motion.div
+					width={300}
+					height={200}
+					onHoverStart={handleHover}
+					onHoverEnd={handleHover}
+				>
+					<Typography component={motion.div} animate={top} sx={headerStyle}>
+						DIGITAL
+					</Typography>
+					<Typography
+						component={motion.div}
+						animate={bottom}
+						sx={headerStyle}
+						marginLeft={13}
 					>
-						<DigitalAscent />
-					</motion.div>
-				)}
-			</Container>
-		</>
+						ASCENT
+					</Typography>
+				</motion.div>
+			)}
+		</Container>
 	);
 }
 
 export default Homepage;
 
-function DigitalAscent() {
-	const digitalAscent = ['DIGITAL', 'ASCENT'];
-
-	return (
-		<>
-			<Grid container flexDirection='column' overflow
->
-				<Box>
-					<Grid container flexDirection='row'>
-						{Array.from(digitalAscent[0]).map((e) => (
-							<motion.div
-								drag
-							>
-								<Typography
-
-									lineHeight={0.88}
-									fontFamily='Monoton'
-									variant='h1'
-									fontSize={'clamp(30px, 15vw, 105px)'}
-									fontWeight={400}
-									cursor='pointer'
-								>
-									{e}
-								</Typography>
-							</motion.div>
-						))}
-					</Grid>
-				</Box>
-				<Box>
-					<Grid container flexDirection='row'>
-						{Array.from(digitalAscent[1]).map((e) => (
-							<motion.div
-								drag
-							>
-								<Typography
-									lineHeight={0.88}
-									fontFamily='Monoton'
-									variant='h1'
-									fontSize={'clamp(30px, 15vw, 105px)'}
-									fontWeight={400}
-									cursor='pointer'
-								>
-									{e}
-								</Typography>
-							</motion.div>
-						))}
-					</Grid>
-				</Box>
-			</Grid>
-		</>
-	);
-}
+const headerStyle = {
+	lineHeight: 0.88,
+	fontFamily: 'Monoton',
+	variant: 'h1',
+	fontSize: 'clamp(30px, 15vw, 105px)',
+	fontWeight: 400,
+	cursor: 'pointer',
+};
