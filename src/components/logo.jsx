e1317a770a3372cbafe1d8cd233e1b62273daa26
gnayiqnat <gnayiqnat@gmail.com>
2024-02-07 11:42:44 +0800

    Fix logo not appearing in 404 page

--- src/components/logo.jsx
+++ src/components/logo.jsx
@@ -1,25 +1,29 @@
 import { Link as RouterLink } from 'react-router-dom';
-import { useMediaQuery } from "react-responsive";
+import { useMediaQuery } from 'react-responsive';
 
-const logo = './logo/logo-light.png';
-const logo_dark = './logo/logo-dark.png';
+const logo = '/logo/logo-light.png';
+const logo_dark = '/logo/logo-dark.png';
 
 export default function Logo(props) {
 	const isMobile = useMediaQuery({ query: '(max-width: 768px)' });
 
 	return (
-		<>{ props.isNotification ? <img
-			src={props.themeMode === 'dark' ? logo : logo_dark}
-			style={{ marginLeft: 5, marginTop: 10 }}
-			width={25}
-		/> :
-			<RouterLink to='/'>
+		<>
+			{props.isNotification ? (
 				<img
 					src={props.themeMode === 'dark' ? logo : logo_dark}
 					style={{ marginLeft: 5, marginTop: 10 }}
-					height={isMobile ? 60 : 80}
+					width={25}
 				/>
-			</RouterLink>}
+			) : (
+				<RouterLink to='/'>
+					<img
+						src={props.themeMode === 'dark' ? logo : logo_dark}
+						style={{ marginLeft: 5, marginTop: 10 }}
+						height={isMobile ? 60 : 80}
+					/>
+				</RouterLink>
+			)}
 		</>
 	);
 }
---
+++ src/pages/games page/template.jsx
@@ -0,0 +1,18 @@
+import { Box } from "@mui/material";
+
+export default function GamePageTemplate() {
+    
+    return (
+        <>
+            
+
+
+
+
+            {/* Background peek */}
+            <Box>
+                <img src='/dte-peek.png' />
+            </Box>
+        </>
+    )
+}
