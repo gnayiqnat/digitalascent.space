6cfe80afd56afa5165b336e11e28ef7ea7995533
gnayiqnat <gnayiqnat@gmail.com>
2024-02-07 11:01:35 +0800

    Create a loading screen for credits page

--- src/pages/credits.jsx
+++ src/pages/credits.jsx
@@ -1,30 +1,6 @@
-import {
-	Typography,
-	Button,
-	IconButton,
-	Fab,
-	Box,
-	AppBar,
-	Toolbar,
-	Drawer,
-	List,
-	ListItem,
-	ListItemIcon,
-	ListItemText,
-	Divider,
-	Grid,
-	Paper,
-	Card,
-	CardHeader,
-	CardMedia,
-	CardContent,
-	CardActions,
-	CssBaseline,
-	Avatar,
-	Container,
-} from '@mui/material';
-import { MotionConfig, motion } from 'framer-motion';
-import { useEffect } from 'react';
+import { Typography, Box, Grid, LinearProgress, Avatar } from '@mui/material';
+import { motion } from 'framer-motion';
+import { useEffect, useRef, useState } from 'react';
 import { useMediaQuery } from 'react-responsive';
 
 const team = [
@@ -49,7 +25,7 @@ const team = [
 	{
 		avatar: './pfp/ben.jpg',
 		username: 'benjamintjm',
-		roles: 'Game tester',
+		roles: 'Game Tester',
 		links: 'https://github.com/benjamintjm',
 	},
 	{
@@ -65,9 +41,86 @@ const team = [
 
 export default function Credits(props) {
 	const isMobile = useMediaQuery({ query: '(max-width: 768px)' });
-	
+	const [allImageshasLoaded, setAllImagesHasLoaded] = useState(false);
+	const numberOfImgLoaded = useRef(0);
+	const [loadingProgress, setLoadingProgress] = useState(1);
+
+	function addNumLoaded() {
+		if (document.cookie.includes('creditsImgLoaded=true')) {
+			return;
+		} else {
+			numberOfImgLoaded.current++;
+		}
+	}
+	useEffect(() => {
+		let numberOfTeamWithAvatar = 0
+		team.map(e => {
+			if (e.avatar) {
+				numberOfTeamWithAvatar++
+			}
+		})
+		if (document.cookie.includes('creditsImgLoaded=true')) {
+			setAllImagesHasLoaded(true);
+			return;
+		} else {
+			const timer = setInterval(() => {
+				setLoadingProgress((oldProgress) => {
+					if (oldProgress === 100) {
+						if (numberOfImgLoaded.current === numberOfTeamWithAvatar) {
+							setAllImagesHasLoaded(true);
+							document.cookie =
+								'creditsImgLoaded=true; SameSite=Strict; Secure';
+						}
+						return 100;
+					}
+					return (numberOfImgLoaded.current / numberOfTeamWithAvatar) * 100;
+				});
+			}, 500);
+			return () => {
+				clearInterval(timer);
+			};
+		}
+	}, []);
+
 	return (
 		<>
+			{!allImageshasLoaded && (
+				<>
+					<Box
+						sx={{
+							width: '100vw',
+							height: '100dvh',
+							position: 'absolute',
+							disabled: 'disabled', // disable cursor passthrough
+							zIndex: 2,
+							left: 0,
+							top: 0,
+							backgroundColor: 'background.paper',
+						}}
+					>
+						<Box
+							sx={{
+								display: 'flex',
+								flexDirection: 'column',
+								justifyContent: 'center',
+								alignItems: 'center',
+								height: '100dvh',
+							}}
+						>
+							<LinearProgress
+								variant='determinate'
+								value={loadingProgress}
+								sx={{
+									display: 'inline-block',
+									width: '40vw',
+									height: '7px',
+									borderRadius: '30px',
+								}}
+							/>
+						</Box>
+					</Box>
+				</>
+			)}
 			<motion.div
 				variants={props.routeVariants}
 				initial='initial'
@@ -97,7 +150,7 @@ export default function Credits(props) {
 									style={{ textDecoration: 'none' }}
 								>
 									<motion.div
-										initial={{ opacity: 1, scale: isMobile ? 0.9 : 1}}
+										initial={{ opacity: 1, scale: isMobile ? 0.9 : 1 }}
 										whileHover={{ opacity: e.links ? 0.5 : 1 }}
 										style={{
 											display: 'flex',
@@ -110,14 +163,28 @@ export default function Credits(props) {
 											whiteSpace: 'nowrap',
 										}}
 									>
-										<Avatar
-											src={e.avatar}
-											sx={{
-												width: 100,
-												height: 100,
-												marginBottom: 2,
-											}}
-										/>
+										{e.avatar ? (
+											<img
+												src={e.avatar}
+												onLoad={() => {
+													addNumLoaded();
+												}}
+												style={{
+													borderRadius: '50%',
+													width: 100,
+													height: 100,
+													marginBottom: 2,
+												}}
+											/>
+										) : (
+											<Avatar
+												style={{
+													width: 100,
+													height: 100,
+													marginBottom: 2,
+												}}
+											/>
+										)}
 										<Typography
 											align='center'
 											sx={{
